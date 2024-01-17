af7273647454068e266e98086f8a4bc27f02e14b
hunghx <baby10051999@gmail.com>
2024-01-17 20:31:34 +0700

    update

--- src/main/java/ra/thymeleaf/controller/HomeController.java
+++ src/main/java/ra/thymeleaf/controller/HomeController.java
@@ -9,9 +9,10 @@ import ra.thymeleaf.model.Student;
 public class HomeController {
     @RequestMapping({"/","/home"}) // đường dẫn ""
     public String home(Model model){
-        model.addAttribute("name","Hồ Xuaan Hùng");
+        model.addAttribute("name","hung");
         model.addAttribute("age",24);
         model.addAttribute("student",new Student(1,"Hồ Xuân Hùng",24));
+        model.addAttribute("array",new int[]{1,2,3,4,5,6});
         return "home";
     }
 }
